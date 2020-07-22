0bb1ce694e77bebf6f6b81bb289dc503c444f46b
Spiky <mrmrmtarr@gmail.com>
2020-07-23 00:31:57 +0200

    Update sh_settings.lua

--- fsn_main/server_settings/sh_settings.lua
+++ fsn_main/server_settings/sh_settings.lua
@@ -5,7 +5,7 @@ GM['currency'] = {}
 GM['version'] = '2.1.0'; -- Do not change this value
 
 GM['rules'] = { -- The GUI is desgined to support 7 rules. Adding more is not recommended.
-	[1] = 'Join Our Discord!',
+	[1] = 'Join Our Discord @ discord.io/sannrp!',
 	[2] = 'Cheating, exploiting or hacking of any kind is not permitted, this includes external crosshairs.',
 	[3] = 'Do not RDM/VDM - You have to have initiated in roleplay with the individual first.',
 	[4] = 'Twitch TOS - Do not intentionally violate Twitch terms of service while playing.',
@@ -17,5 +17,5 @@ GM['rules'] = { -- The GUI is desgined to support 7 rules. Adding more is not re
 GM['server'].name = ':FSN: Framework Dev Server'; -- Server/community name
 GM['server'].url = 'https://fsn.rocks'; -- URL of server (also ban screen)
 
-GM['currency'].icon = '$';
-GM['currency'].name = 'USD';
+GM['currency'].icon = 'R';
+GM['currency'].name = 'ZAR';
